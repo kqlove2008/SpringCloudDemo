9dbe2929e9055482b1d0972d071a2c0ccff8239a
lqkq304kq@163.com <lqkq304kq@163.com>
2018-06-20 14:53:53 +0800

    代码中添加必要注释说明

--- eurekaServer1/src/main/java/com/leon/cloud/eurekaserver/EurekaServerApplication.java
+++ eurekaServer1/src/main/java/com/leon/cloud/eurekaserver/EurekaServerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/*
+	添加注释
+*/
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
